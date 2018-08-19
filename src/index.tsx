a27542fafc6b12639a647e4a75af1e1e9509cdc5
Frederic Collonval <fcollonval@gmail.com>
2018-08-19 18:41:57 +0200

    Prettify

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,24 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-import { LabToastContainer, INotification } from './ToastJupyterLab';
+import { LabToastContainer, INotification } from "./ToastJupyterLab";
 
-import {
-  JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
+import { JupyterLab, JupyterLabPlugin } from "@jupyterlab/application";
 
-import '../style/index.css';
-
+import "../style/index.css";
 
 /**
  * Initialization data for the jupyterlab_toastify extension.
  */
 const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_toastify',
+  id: "jupyterlab_toastify",
   autoStart: true,
   activate: (app: JupyterLab) => {
     app.restored.then(() => {
-      const node = document.createElement('div');
+      const node = document.createElement("div");
       document.body.appendChild(node);
       ReactDOM.render(<LabToastContainer />, node);
-  
+
       /* For testing
       let id = INotification.inProgress('Task in progress!')
       setTimeout(INotification.update, 2500, id, 'Updating task...');
@@ -33,10 +30,10 @@ const extension: JupyterLabPlugin<void> = {
       setTimeout(INotification.success, 1500, 'Success');
       setTimeout(INotification.notify, 2000, 'Default');
       */
-    });    
+    });
   }
 };
 
 export { INotification };
-export { toast } from 'react-toastify';
+export { toast } from "react-toastify";
 export default extension;
