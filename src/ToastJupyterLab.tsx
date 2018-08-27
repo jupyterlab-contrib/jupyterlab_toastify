71759cfaeac1af82c3784773a051278870ed46af
Frederic Collonval <fcollonval@gmail.com>
2018-08-27 13:55:58 +0200

    Make update input an object

--- src/ToastJupyterLab.tsx
+++ src/ToastJupyterLab.tsx
@@ -27,50 +27,52 @@ export class LabToastContainer extends React.Component<ToastContainerProps> {
 }
 
 export namespace INotification {
-  export const error = (message: string): number =>
+  export const error = (message: React.ReactNode): number =>
     toast(message, {
       type: "error",
       className: "jp-toastContainer-error",
       autoClose: false
     });
 
-  export const warning = (message: string): number =>
+  export const warning = (message: React.ReactNode): number =>
     toast(message, {
       type: "warning",
       className: "jp-toastContainer-warning",
       autoClose: false
     });
 
-  export const info = (message: string): number =>
+  export const info = (message: React.ReactNode): number =>
     toast(message, {
       type: "info",
       className: "jp-toastContainer-info"
     });
 
-  export const success = (message: string): number =>
+  export const success = (message: React.ReactNode): number =>
     toast(message, {
       type: "success",
       className: "jp-toastContainer-success"
     });
 
-  export const inProgress = (message: string): number =>
+  export const inProgress = (message: React.ReactNode): number =>
     toast(message, {
       type: "default",
       className: "jp-toastContainer-inprogress",
       autoClose: false
     });
 
-  export const update = function(
+  export interface IUpdate {
     toastId: number,
-    message: string,
+    message: React.ReactNode,
     type?: ToastType,
     autoClose?: number
-  ) {
-    let options: UpdateOptions = { render: message };
-    if (type !== undefined) options.type = type;
-    if (autoClose !== undefined) options.autoClose = autoClose;
+  }
 
-    toast.update(toastId, options);
+  export const update = function(args: IUpdate) {
+    let options: UpdateOptions = { render: args.message };
+    if (args.type !== undefined) options.type = args.type;
+    if (args.autoClose !== undefined) options.autoClose = args.autoClose;
+
+    toast.update(args.toastId, options);
   };
 
   export const notify = (
